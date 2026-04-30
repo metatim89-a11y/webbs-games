61c2ffc55621004999332c91fa7b3422dbd6e2d8
metatim89-a11y <Meta.tim.89@Gmail.com>
2026-04-30 17:15:44 -0500

    Fix corrupted cssText properties and restore Sorry board rendering (v1.1.3)

--- version-update.sh
+++ version-update.sh
@@ -11,15 +11,12 @@ NEW_VER=$1
 echo "Updating project to version v$NEW_VER..."
 
 # 1. Update the visible version tag in HTML files
-# Looks for <div id="version-tag">v...</div>
 find . -name "*.html" -exec sed -i "s|<div id=\"version-tag\">v[0-9.]*</div>|<div id=\"version-tag\">v$NEW_VER</div>|g" {} +
 
-# 2. Update CSS links with cache busting
-# Looks for href="...style.css?v=..." or href="...style.css"
-find . -name "*.html" -exec sed -i "s|style.css\([?v=0-9.]*\)\?|style.css?v=$NEW_VER|g" {} +
+# 2. Update CSS links with cache busting (Strictly target href)
+find . -name "*.html" -exec sed -i "s|href=\"\([^\"]*\)style.css[^\"]*\"|href=\"\1style.css?v=$NEW_VER\"|g" {} +
 
-# 3. Update JS imports with cache busting
-# Looks for src="...js?v=..." or src="...js" (excluding external unpkg links)
-find . -name "*.html" -exec sed -i "/unpkg.com/! s|\.js\([?v=0-9.]*\)\?|.js?v=$NEW_VER|g" {} +
+# 3. Update JS imports with cache busting (Strictly target src, avoid unpkg)
+find . -name "*.html" -exec sed -i "/unpkg.com/! s|src=\"\([^\"]*\)\.js[^\"]*\"|src=\"\1.js?v=$NEW_VER\"|g" {} +
 
 echo "Done! All files updated to v$NEW_VER."
